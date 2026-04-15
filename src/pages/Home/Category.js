9e04cec5760769ecea505e3a90b0883793b81c02
danh05341 <danh05341@gmail.com>
2026-04-15 13:22:57 +0700

    feat: trigger deploy

--- src/pages/Home/Category.js
+++ src/pages/Home/Category.js
@@ -2,7 +2,6 @@ import Banner from "./Banner"
 
 const Category = () => {
   return (
-    //section-category
     <div className="flex w-full ">
         <div className="w-[1200px]  m-auto ">
             <div className="mx-[15px] ">
